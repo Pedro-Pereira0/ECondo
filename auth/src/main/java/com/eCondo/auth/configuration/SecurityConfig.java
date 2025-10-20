91039a1f16c79b36dba2d44e6cfb19388b38f721
Pedro-Pereira0 <pedrodanielmilheirosilvapereira@outlook.pt>
2025-10-20 16:59:18 +0100

    Finished the securityConfig file.

--- auth/src/main/java/com/eCondo/auth/configuration/SecurityConfig.java
+++ auth/src/main/java/com/eCondo/auth/configuration/SecurityConfig.java
@@ -1,14 +1,44 @@
 package com.eCondo.auth.configuration;
 
-import java.security.interfaces.RSAKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import static java.lang.String.format;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,10 +48,11 @@ import lombok.RequiredArgsConstructor;
 @EnableConfigurationProperties
 @RequiredArgsConstructor
 public class SecurityConfig {
-    //Repo
-    //private final UserRepository userRepo;
+
+    private final UserRepository userRepo;
+
     @Value("${jwt.private.key}")
-    private RSAPrivateKey rsaprivateKey;
+    private RSAPrivateKey rsaPrivateKey;
 
     @Value("classpath:rsa.public.key")
     private RSAPublicKey rsaPublicKey;
@@ -32,7 +63,99 @@ public class SecurityConfig {
 	@Value("${springdoc.swagger-ui.path}")
 	private String swaggerPath;
 
+    @Bean
+    public AuthenticationManager authenticationManager(final UserDetailsService userDetailsService, final PasswordEncoder passwordEncoder){
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepo.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException(format("User: %s, not found", username)));
+    }
     
-    
-    
+    // Set password encoding schema
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        // Enable CORS and disable CSRF
+		http = http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable());
+
+		// Set session management to stateless
+		http = http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Set unauthorized requests exception handler
+		http = http.exceptionHandling(
+				exceptions -> exceptions.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+						.accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
+
+		// Set permissions on endpoints
+        http.authorizeHttpRequests()
+            // Swagger endpoints must be public
+				.requestMatchers("/").permitAll().requestMatchers(format("%s/**", restApiDocPath)).permitAll()
+				.requestMatchers(format("%s/**", swaggerPath)).permitAll()
+            // public endpoints
+
+            // private endpoints
+            .requestMatchers("/api/admin/user/**").hasRole(Role.ADMIN) // user management
+            .anyRequest().authenticated()
+
+            .and()
+                .headers().frameOptions().disable();
+
+        http
+            // Set up oauth2 resource server
+            .httpBasic(Customizer.withDefaults())
+            .oauth2ResourceServer().jwt();
+
+
+		return http.build(); 
+    }
+
+    // Used by JwtAuthenticationProvider to generate JWT tokens
+	@Bean
+	public JwtEncoder jwtEncoder() {
+		final JWK jwk = new RSAKey.Builder(this.rsaPublicKey).privateKey(this.rsaPrivateKey).build();
+		final JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+		return new NimbusJwtEncoder(jwks);
+	}
+
+    // Used by JwtAuthenticationProvider to decode and validate JWT tokens
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withPublicKey(this.rsaPublicKey).build();
+	}
+
+	// Extract authorities from the roles claim
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+		jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+		final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+		return jwtAuthenticationConverter;
+	}
+
+
+	// Used by spring security if CORS is enabled.
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
